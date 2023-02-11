bc6b51f21f29ff01db9f56486a8e78544b00161e
Uxoa <palomita@gmail.com>
2023-02-11 22:40:30 +0100

    working on show blade

--- resources/views/student/show.blade.php
+++ resources/views/student/show.blade.php
@@ -7,43 +7,84 @@
 @section('content')
     <section class="content container-fluid">
         <div class="studentCard">
-      
-            <img src="{{$user->image}}" alt="adriana" class="imageStudent">
+
+            <img src="{{$student->image}}" alt="adriana" class="imageStudent">
         
         <div class="infoStudent">
-            <strong>Name:</strong>  {{ $student->name }}</p>
-            <p>Curso: </p>
-            <p>E-mail: {{$user->email}}</p>
+            <strong>Name:</strong> {{ $student->name }}<br/>
+        <strong>Grades Id:</strong> {{ $student->grades_id }}
         </div>
+</div>
+
+
+<div class="allT">
+    <table class="firstT box tableHome table-striped text-center">
+            
+            <thead class="tableHead">
+                <tr>
+                    <td rowspan="2" class="align-middle"></td>
+                    <td colspan="4">PRIMER TRIMESTRE</td>
+
+                </tr>
+                <tr class="tableNotes"> 
+                    <td>Nota 1</td>
+                    <td>Nota 2</td>
+                    <td>Nota 3</td>
+                    <td>Final</td>
+
+                </tr>
+            </thead>
+            
+            <tbody>
+                <tr>
+                    <td>Lengua</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Inglés</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Matemáticas</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Historia</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Geografía</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+</table>
 </div>
 
 
 
 
 
-
-
                     <div class="card-header">
-                        <div class="float-left">
-                            <span class="card-title">Show Student</span>
-                        </div>
                         <div class="float-right">
                             <a class="btn btn-primary" href="{{ route('students.index') }}"> Back</a>
                         </div>
                     </div>
 
-                    <div class="card-body">
-                        
-                        <div class="form-group">
-                            <strong>Name:</strong>
-                            {{ $student->name }}
-                        </div>
-                        <div class="form-group">
-                            <strong>Grades Id:</strong>
-                            {{ $student->grades_id }}
-                        </div>
-
-        
-        </div>
+    
     </section>
 @endsection
