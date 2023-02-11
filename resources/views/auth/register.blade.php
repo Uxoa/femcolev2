d20f6756a81c317e1e06bc9ba2fbd4e1f6aee6e3
Uxoa <palomita@gmail.com>
2023-02-11 10:28:08 +0100

    working on register blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
                             </div>
 
-                            <div class="col-md-6">
+                            <div class="colInput">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
@@ -24,11 +24,11 @@
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
+                        <div class="form-group">
+                            <div class="colLabel">
+                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            </div>
+                            <div class="colInput">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
@@ -39,10 +39,11 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
+                        <div class="form-group">
+                            <div class="colLabel">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
+                            </div>
+                            <div class="colInput">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
@@ -53,16 +54,17 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
+                        <div class="form-group">
+                            <div class="colLabel">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
+                            </div>
+                            <div class="colInput">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="form-group">
+                            <div class="colInput">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
